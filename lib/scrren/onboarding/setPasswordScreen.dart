da591c089be0991355722f7539367eda9f4e0975
RimonChakma <rimon.chakma880@gmail.com>
2025-06-28 23:29:42 +0600

    set password screen

--- lib/scrren/onboarding/setPasswordScreen.dart
+++ lib/scrren/onboarding/setPasswordScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../style/style.dart';
+
 class setPasswordScreen extends StatefulWidget {
   const setPasswordScreen({super.key});
 
@@ -11,7 +13,31 @@ class _setPasswordScreenState extends State<setPasswordScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+        body: Stack(
+          children: [
+            ScreenBackground(context),
+            Padding(padding: EdgeInsets.all(30),child:Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Set Password",style: head1Text(colorDarkBlue),),
+                    SizedBox(height: 5,),
+                    Text("Minimum length password 8 character with letter and number combination",style: head2Text(colorLightGray),),
+                    SizedBox(height: 20,),
+                    TextFormField(decoration: AppInputDecoration("Password"),),
+                    SizedBox(height: 20,),
+                    TextFormField(decoration: AppInputDecoration("Conform password"),),
+                    SizedBox(height: 20,),
+                    Container(child: ElevatedButton(onPressed: (){},style: AppButtonStyle(),
+                        child: SuccessButtonChild("conform")),)
+                  ],
+                ),
+              ),
+            ),)
+          ],
+        )
     );
   }
 }
