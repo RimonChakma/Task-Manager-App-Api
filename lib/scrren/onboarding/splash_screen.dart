60bfd9d808673f880b1eaf058e3d911345a719aa
RimonChakma <rimon.chakma880@gmail.com>
2025-06-28 20:14:36 +0600

    created splash screen

--- lib/scrren/onboarding/splash_screen.dart
+++ lib/scrren/onboarding/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_api/style/style.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +11,13 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(children: [
+        ScreenBackground(context),
+        Container(
+          padding: EdgeInsets.all(30),
+          child: Center(child: Image.asset("assets/images/logo.png",alignment: Alignment.center,height: 200,width: 200,)),)
+      ],),
+    );
   }
 }
