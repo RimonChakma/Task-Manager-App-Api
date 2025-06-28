c64d85dadafa79dc645fe9eb104c80fc642e599e
RimonChakma <rimon.chakma880@gmail.com>
2025-06-28 10:14:14 +0600

    add routes Screen

---
+++ lib/scrren/onboarding/emailVerificationScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Emailverificationscreen extends StatefulWidget {
+  const Emailverificationscreen({super.key});
+
+  @override
+  State<Emailverificationscreen> createState() => _EmailverificationscreenState();
+}
+
+class _EmailverificationscreenState extends State<Emailverificationscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/scrren/onboarding/pinVerificationScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PinverificationScreen extends StatefulWidget {
+  const PinverificationScreen({super.key});
+
+  @override
+  State<PinverificationScreen> createState() => _PinverificationScreenState();
+}
+
+class _PinverificationScreenState extends State<PinverificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
