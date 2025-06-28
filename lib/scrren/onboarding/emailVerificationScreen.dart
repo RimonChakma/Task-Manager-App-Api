e0a8b985904d27d3c5abcccf1ee122fe7b15e470
RimonChakma <rimon.chakma880@gmail.com>
2025-06-28 23:29:00 +0600

    add emailVerificationScreen

--- lib/scrren/onboarding/emailVerificationScreen.dart
+++ lib/scrren/onboarding/emailVerificationScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../style/style.dart';
+
 class Emailverificationscreen extends StatefulWidget {
   const Emailverificationscreen({super.key});
 
@@ -10,6 +12,30 @@ class Emailverificationscreen extends StatefulWidget {
 class _EmailverificationscreenState extends State<Emailverificationscreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Stack(
+          children: [
+            ScreenBackground(context),
+            Padding(padding: EdgeInsets.all(30),child:Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("verify your email address",style: head1Text(colorDarkBlue),),
+                    SizedBox(height: 5,),
+                    Text("A 6 digit verification pin will sent to your email address",style: head2Text(colorLightGray),),
+                    SizedBox(height: 20,),
+                    TextFormField(decoration: AppInputDecoration("email address"),),
+                    SizedBox(height: 20,),
+                    Container(child: ElevatedButton(onPressed: (){},style: AppButtonStyle(),
+                        child: SuccessButtonChild("verify")),)
+                  ],
+                ),
+              ),
+            ),)
+          ],
+        )
+    );
   }
 }
