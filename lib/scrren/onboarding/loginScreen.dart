97264b680cc32ef259fbbdb781cad49cff8f70f4
USER PC <rimon.chakma880@gmial.com>
2025-06-29 21:29:50 +0600

    add login process

--- lib/scrren/onboarding/loginScreen.dart
+++ lib/scrren/onboarding/loginScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_api/scrren/onboarding/registration_screen.dart';
 import 'package:task_manager_api/style/style.dart';
+
+import '../../api/api_client.dart';
 
 class Loginscreen extends StatefulWidget {
   const Loginscreen({super.key});
@@ -9,10 +12,24 @@ class Loginscreen extends StatefulWidget {
 }
 
 class _LoginscreenState extends State<Loginscreen> {
+
+  Map<String,String> formValues = {"email":"","password":""};
+  bool isLoading = false;
+
+  inputOnChange (mapKey,textValue) {
+    setState(() {
+      formValues.update(mapKey, (value) => textValue,);
+    });
+  }
+
+  final formState = GlobalKey<FormState>();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: isLoading? Center(child: CircularProgressIndicator(),):
+      Stack(
         children: [
           ScreenBackground(context),
           Padding(padding: EdgeInsets.all(30),child:Container(
@@ -24,12 +41,54 @@ class _LoginscreenState extends State<Loginscreen> {
                 SizedBox(height: 5,),
                 Text("Login your account",style: head2Text(colorLightGray),),
                 SizedBox(height: 20,),
-                TextFormField(decoration: AppInputDecoration("email address"),),
-                SizedBox(height: 20,),
-                TextFormField(decoration: AppInputDecoration("password"),),
-                SizedBox(height: 20,),
-                Container(child: ElevatedButton(onPressed: (){},style: AppButtonStyle(),
-                    child: SuccessButtonChild("login")),)
+
+                Form(
+                    key: formState,
+                    child: Column(children: [
+                      TextFormField(
+
+                        onChanged: (textValue) {
+                          inputOnChange("email", textValue);
+                        },
+                        validator: (value) {
+                          if(value == null || value.isEmpty){
+                            return "please enter your email address";
+                          }
+                        },
+                        decoration: AppInputDecoration("email address"),),
+                      SizedBox(height: 20,),
+
+
+                      TextFormField(
+                        validator: (value) {
+                          if(value == null || value.isEmpty){
+                            return "please enter your password";
+                          }
+                        },
+                        onChanged: (textValue) {
+                          inputOnChange("password", textValue);
+                        },
+                        decoration: AppInputDecoration("password"),),
+                      SizedBox(height: 20,),
+
+                      Container(child: ElevatedButton(
+                          onPressed: () async{
+                            if(formState.currentState!.validate()){
+                              setState(() {
+                                isLoading = true;
+                              });
+
+                              var result = await LoginRequest(formValues);
+                              if(result == true){
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => RegistrationScreen(),));
+                              }
+                              setState(() {
+                                isLoading = false;
+                              });
+                            }
+                          },style: AppButtonStyle(),
+                          child: SuccessButtonChild("login")),)
+                    ],)),
               ],
             ),
           ),)
