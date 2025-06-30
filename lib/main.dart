88453a3f2d43e55d80c7207f189dd769fdd9acec
USER PC <rimon.chakma880@gmial.com>
2025-06-30 11:05:20 +0600

    read token initialized

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,39 @@ import 'package:task_manager_api/scrren/onboarding/pinVerificationScreen.dart';
 import 'package:task_manager_api/scrren/onboarding/registration_screen.dart';
 import 'package:task_manager_api/scrren/onboarding/setPasswordScreen.dart';
 import 'package:task_manager_api/scrren/onboarding/splash_screen.dart';
+import 'package:task_manager_api/scrren/task/taskCreateScreen.dart';
+import 'package:task_manager_api/utility/utility.dart';
 
-void main () {
-  runApp(MyApp());
+void main () async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  String token = await readUserData("token");
+
+  if(token == null){
+    runApp(MyApp("/loginScreen"));
+  }else{
+    runApp(MyApp("/newTaskScreen"));
+  }
 }
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+  final String token;
+  MyApp(this.token);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "TaskManager",
-      initialRoute: "/pinVerificationScreen",
+      initialRoute: token,
       routes: {
         "/" : (context)=> SplashScreen(),
         "/setPasswordScreen" : (context)=>setPasswordScreen(),
         "/registrationScreen" : (context)=>RegistrationScreen(),
         "/pinVerificationScreen" : (context)=>PinverificationScreen(),
         "/loginScreen" : (context)=>Loginscreen(),
-        "/emailVerificationScreen" :(context)=>Emailverificationscreen()
+        "/emailVerificationScreen" :(context)=>Emailverificationscreen(),
+        "/newTaskScreen" :(context)=>NewTaskScreen()
       },
     );
   }
